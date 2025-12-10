99ccee284d9dbad897e3f0d52d8bf5ce2c477851
Milan Tol <milantol35@gmail.com>
2025-12-10 09:28:24 +0100

    updated solver

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-#include "Renderer.h"
+#include "renderer.h"
 
 
 int main() {
--- solver.h
+++ solver.h
@@ -7,8 +7,23 @@ class Solver {
 public: 
     Solver() = default;
 
+    void update(){
+        apply_gravity();
+        update_particles();
+    }
+
 private:
     float step_dt = 1/60;
     std::vector<Particle> particles;
     sf::Vector2f gravity = {0.0f, -1000.0f};
+
+    void apply_gravity() {
+        for (auto &particle_ : particles)
+            particle_.accelerate(gravity);
+    }
+
+    void update_particles() {
+        for (auto &particle_ : particles)
+            particle_.accelerate(gravity);
+    }
 };
